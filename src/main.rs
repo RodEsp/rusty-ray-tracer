f4643407137a0ade7d3067fc9b0af0d5a7ac926e
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-05-02 12:56:27 -0400

    in-progress: try to add buffer to pass camera coords to compute shader

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,23 @@ extern crate sdl2;
 extern crate vulkano;
 
 use sdl2::{event::Event, keyboard::Keycode};
-use std::sync::Arc;
+use std::{collections::BTreeMap, sync::Arc};
 use vulkano::{
+    buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage},
     command_buffer::{
         allocator::StandardCommandBufferAllocator, AutoCommandBufferBuilder, CommandBufferUsage,
     },
     descriptor_set::{
-        allocator::StandardDescriptorSetAllocator, PersistentDescriptorSet, WriteDescriptorSet,
+        allocator::StandardDescriptorSetAllocator,
+        layout::{DescriptorSetLayoutBinding, DescriptorSetLayoutCreateInfo, DescriptorType},
+        PersistentDescriptorSet, WriteDescriptorSet,
     },
     device::{Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo, QueueFlags},
     image::{view::ImageView, ImageUsage},
     instance::{Instance, InstanceCreateInfo, InstanceExtensions},
+    memory::allocator::{AllocationCreateInfo, MemoryUsage, StandardMemoryAllocator},
     pipeline::{ComputePipeline, Pipeline, PipelineBindPoint},
+    shader::ShaderStages,
     swapchain::{
         AcquireError, Surface, SurfaceApi, Swapchain, SwapchainCreateInfo, SwapchainPresentInfo,
     },
@@ -24,7 +29,7 @@ use vulkano::{
 const SCREEN_WIDTH: u32 = 1920;
 const SCREEN_HEIGHT: u32 = 1080;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, BufferContents)]
 #[repr(C)]
 struct Camera {
     position: [f32; 3],
@@ -77,7 +82,7 @@ fn main() {
         )
     };
 
-    // Create a device
+    // Find a physical device (GPU)
     let physical_device = instance
         .enumerate_physical_devices()
         .expect("could not enumerate devices")
@@ -93,6 +98,8 @@ fn main() {
                 .contains(QueueFlags::COMPUTE)
         })
         .expect("couldn't find a compute queue family") as u32;
+
+    // Create a device from the physical device
     let (device, mut queues) = Device::new(
         Arc::clone(&physical_device),
         DeviceCreateInfo {
@@ -115,17 +122,15 @@ fn main() {
         device.physical_device().properties().device_name
     );
 
+    // Create a swapchain to render to the results of the compute shader
     let surface_capabilities = physical_device
         .surface_capabilities(&surface, Default::default())
         .expect("failed to get surface capabilities");
-
     let composite_alpha = surface_capabilities
         .supported_composite_alpha
         .into_iter()
         .next()
         .unwrap();
-
-    // Create a swapchain to render to the results of the compute shader
     let (swapchain, images) = Swapchain::new(
         Arc::clone(&device),
         Arc::new(surface),
@@ -151,22 +156,64 @@ fn main() {
     }
     let shader = cs::load(Arc::clone(&device)).expect("failed to create shader module");
 
+    fn replace_create_info(mut create_info: &mut [DescriptorSetLayoutCreateInfo]) {
+        let mut ray_tracing_btree_map = BTreeMap::new();
+        ray_tracing_btree_map.insert(
+            0,
+            DescriptorSetLayoutBinding {
+                stages: ShaderStages::COMPUTE,
+                ..DescriptorSetLayoutBinding::descriptor_type(DescriptorType::StorageImage)
+            },
+        );
+
+        let mut camera_btree_map = BTreeMap::new();
+        camera_btree_map.insert(
+            0,
+            DescriptorSetLayoutBinding {
+                stages: ShaderStages::COMPUTE,
+                ..DescriptorSetLayoutBinding::descriptor_type(DescriptorType::UniformBuffer)
+            },
+        );
+
+        let set_layout_create_infos = [
+            // The first descriptor set is for the ray traced image buffer
+            DescriptorSetLayoutCreateInfo {
+                bindings: ray_tracing_btree_map,
+                ..Default::default()
+            },
+            // The second descriptor set is for the camera buffer
+            DescriptorSetLayoutCreateInfo {
+                bindings: camera_btree_map,
+                ..Default::default()
+            },
+        ];
+        let mut set_layout_create_infos_slice = set_layout_create_infos.as_slice();
+        create_info = &mut set_layout_create_infos_slice
+    }
+
     // Create a compute pipeline to run the shader
     let compute_pipeline = ComputePipeline::new(
         Arc::clone(&device),
         shader.entry_point("main").unwrap(),
         &(),
         None,
-        |_| {},
+        replace_create_info,
+        // |_| (),
     )
     .expect("failed to create compute pipeline");
 
+    // println!(
+    //     "Compute Pipeline Set Layouts: {:#?}",
+    //     compute_pipeline.layout().set_layouts()
+    // );
+
+    // Create an SDL event pump to handle events while the window is open
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     'running: loop {
         let camera = Camera::new([0.0, 0.0, 0.0], [0.0, 0.0, -1.0], [0.0, 1.0, 0.0]);
 
-        // Acquire the next image from the swapchain`
+        // Acquire the next image from the swapchain
         let (image_index, _suboptimal_acquisition, acquire_future) =
             match vulkano::swapchain::acquire_next_image(Arc::clone(&swapchain), None) {
                 Ok(result) => result,
@@ -178,12 +225,18 @@ fn main() {
             };
         let image = &images[image_index as usize];
 
-        // Define the descriptor set for the compute shader
+        println!("Image Index: {}", image_index);
+        // println!("Image: {:#?}", image);
+
+        let command_buffer_allocator =
+            StandardCommandBufferAllocator::new(Arc::clone(&device), Default::default());
+
+        // Create the descriptor set for the ray tracing buffer
         let view = ImageView::new_default(Arc::clone(&image)).unwrap();
-
         let descriptor_set_allocator = StandardDescriptorSetAllocator::new(Arc::clone(&device));
-
         let layout = compute_pipeline.layout().set_layouts().get(0).unwrap();
+        // println!("Layout: {:#?}", layout);
+
         let descriptor_set = PersistentDescriptorSet::new(
             &descriptor_set_allocator,
             Arc::clone(&layout),
@@ -191,9 +244,7 @@ fn main() {
         )
         .unwrap();
 
-        let command_buffer_allocator =
-            StandardCommandBufferAllocator::new(Arc::clone(&device), Default::default());
-        // Create a command buffer that runs the compute shader and copies the result to the swapchain image
+        // Create a buffer builder for the ray tracing image buffer
         let mut builder = AutoCommandBufferBuilder::primary(
             &command_buffer_allocator,
             queue.queue_family_index(),
@@ -209,10 +260,59 @@ fn main() {
                 0,
                 descriptor_set,
             )
-            .dispatch([SCREEN_WIDTH / 8, SCREEN_HEIGHT / 8, 1])
+            .dispatch([SCREEN_WIDTH, SCREEN_HEIGHT, 1])
             .unwrap();
 
+        // Create a command buffer that runs the compute shader and copies the result to the swapchain image
         let command_buffer = builder.build().unwrap();
+
+        // Create camera buffer
+        let camera_buffer_memory_allocator =
+            StandardMemoryAllocator::new_default(Arc::clone(&device));
+        let camera_descriptor_set_allocator =
+            StandardDescriptorSetAllocator::new(Arc::clone(&device));
+        let camera_buffer = Buffer::from_data(
+            &camera_buffer_memory_allocator,
+            BufferCreateInfo {
+                usage: BufferUsage::UNIFORM_BUFFER,
+                ..Default::default()
+            },
+            AllocationCreateInfo {
+                usage: MemoryUsage::Upload,
+                ..Default::default()
+            },
+            camera,
+        )
+        .unwrap();
+        let camera_layout = compute_pipeline.layout().set_layouts().get(1).unwrap();
+        // println!("Camera Layout: {:#?}", camera_layout);
+        let camera_descriptor_set = PersistentDescriptorSet::new(
+            &camera_descriptor_set_allocator,
+            Arc::clone(&camera_layout),
+            [WriteDescriptorSet::buffer(1, camera_buffer)],
+        )
+        .unwrap();
+
+        // Create a buffer builder for the camera buffer
+        let mut camera_buffer_builder = AutoCommandBufferBuilder::primary(
+            &command_buffer_allocator,
+            queue.queue_family_index(),
+            CommandBufferUsage::OneTimeSubmit,
+        )
+        .unwrap();
+        camera_buffer_builder
+            .bind_pipeline_compute(Arc::clone(&compute_pipeline))
+            .bind_descriptor_sets(
+                PipelineBindPoint::Compute,
+                Arc::clone(&compute_pipeline.layout()),
+                0,
+                camera_descriptor_set,
+            )
+            .dispatch([1, 1, 1])
+            .unwrap();
+
+        // Create camera buffer
+        let _camera_buffer = camera_buffer_builder.build().unwrap();
 
         // Submit the command buffer to the device queue
         let future = acquire_future
